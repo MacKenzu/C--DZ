b96c269dbc538949369ca58938bd7259ab1cab4f
Jaroslav <129511791+MacKenzu@users.noreply.github.com>
2023-04-10 12:35:44 +0300

    Update second_task.cs

--- Dz_1/second_task.cs
+++ Dz_1/second_task.cs
@@ -3,21 +3,21 @@
 //44 5 78 -> 78
 //22 3 9 -> 22
 
-// Console.WriteLine("Введите первое число");
-// int number = Convert.ToInt32(Console.ReadLine());
-// Console.WriteLine("Введите второе число");
-// int number2 = Convert.ToInt32(Console.ReadLine());
-// Console.WriteLine("Введите тpeтьe число");
-// int number3 = Convert.ToInt32(Console.ReadLine());
-// if (number > number2)
-//     if (number > number3)
-//         Console.WriteLine("Max={0}", number);
-//     else 
-//         Console.WriteLine("Max={0}", number3);
-// else
-//     if (number2 > number3)
-//         Console.WriteLine("Max={0}", number2);
-//     else
-//         Console.WriteLine("Max={0}", number3);
+ Console.WriteLine("Введите первое число");
+ int number = Convert.ToInt32(Console.ReadLine());
+ Console.WriteLine("Введите второе число");
+ int number2 = Convert.ToInt32(Console.ReadLine());
+ Console.WriteLine("Введите тpeтьe число");
+ int number3 = Convert.ToInt32(Console.ReadLine());
+ if (number > number2)
+     if (number > number3)
+         Console.WriteLine("Max={0}", number);
+     else 
+         Console.WriteLine("Max={0}", number3);
+ else
+     if (number2 > number3)
+         Console.WriteLine("Max={0}", number2);
+     else
+         Console.WriteLine("Max={0}", number3);
         
 
