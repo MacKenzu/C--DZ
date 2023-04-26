e1a69c731d7f3a798cc323c8c595f300e78000b0
MacKenzy <Pa4itsky.jaroslav@gmailcom>
2023-04-26 14:02:45 +0300

    разница между мин и макс

---
+++ Dz_5/Task_3/Program.cs
@@ -0,0 +1,34 @@
+﻿// Задача 38: Задайте массив вещественных чисел. Найдите разницу между максимальным и минимальным элементов массива.
+// [3 7 22 2 78] -> 76
+
+void FillArray (int [] array)
+{
+    for (int i = 0; i < array.Length; i++)
+        {
+            array [i] = new Random().Next(100);
+        }
+}
+int sum(int [] array )
+{
+ int max = array[0];
+ int min = array[0];
+ int result = 0;
+ for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i] > max)
+        {        
+            max = array[i];
+        }
+        else if (array[i] < min)
+        {
+            min = array[i];
+        }
+        
+         result = max - min;
+    
+    }
+return result;
+}
+int [] myArray = new int [new Random().Next(20)];
+FillArray(myArray);
+Console.WriteLine("["+ String.Join(",", myArray)+ "] -> "+sum(myArray));
