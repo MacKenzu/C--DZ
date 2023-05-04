24491c7a6194f3576b3668f4a78c61e45f5a1b2e
unknown <pa4itsky.jaroslav@gmail.com>
2023-05-04 22:21:54 +0300

    среднее арифметическое столбцов массива

---
+++ Dz_7/Task_3/Program.cs
@@ -0,0 +1,33 @@
+﻿Console.WriteLine("Введите размерность двумерного массива М , N");
+int[] array = Console.ReadLine().Split().Select(int.Parse).ToArray();
+int [,] myarray = new int[array[0], array[1]];
+for (int i = 0; i < myarray.GetLength(0); i++)
+{
+    for (int j = 0; j < myarray.GetLength(1); j++)
+    {
+        myarray[i,j] = new Random().Next(10);
+                       
+    }
+
+}
+for (int i = 0; i < myarray.GetLength(0); i++)
+    {
+        Console.Write("[ ");
+        for (int j = 0; j < myarray.GetLength(1); j++)
+        {
+            Console.Write(myarray[i, j] + " ");
+        }
+        Console.Write("]");
+        Console.WriteLine("");
+    }
+
+for (int i = 0; i < myarray.GetLength(0); i++)
+{
+    double avg = 0;
+    for (int j = 0; j < myarray.GetLength(1); j++)
+    {
+        avg = avg + myarray[j,i];
+                       
+    }
+    Console.Write((Math.Round(avg/array[0], 1))+"; ");
+}
