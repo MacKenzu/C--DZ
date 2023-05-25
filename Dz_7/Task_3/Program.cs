c9a6e828042bdc4cc9e347e549064a2169c0a4e7
unknown <pa4itsky.jaroslav@gmail.com>
2023-05-25 20:25:16 +0300

    сто-то

--- Dz_7/Task_3/Program.cs
+++ Dz_7/Task_3/Program.cs
@@ -1,33 +1,33 @@
 ﻿Console.WriteLine("Введите размерность двумерного массива М , N");
 int[] array = Console.ReadLine().Split().Select(int.Parse).ToArray();
-int [,] myarray = new int[array[0], array[1]];
+int[,] myarray = new int[array[0], array[1]];
 for (int i = 0; i < myarray.GetLength(0); i++)
 {
     for (int j = 0; j < myarray.GetLength(1); j++)
     {
-        myarray[i,j] = new Random().Next(10);
-                       
+        myarray[i, j] = new Random().Next(10);
+
     }
 
 }
 for (int i = 0; i < myarray.GetLength(0); i++)
+{
+    Console.Write("[ ");
+    for (int j = 0; j < myarray.GetLength(1); j++)
     {
-        Console.Write("[ ");
-        for (int j = 0; j < myarray.GetLength(1); j++)
-        {
-            Console.Write(myarray[i, j] + " ");
-        }
-        Console.Write("]");
-        Console.WriteLine("");
+        Console.Write(myarray[i, j] + " ");
     }
+    Console.Write("]");
+    Console.WriteLine("");
+}
 
 for (int i = 0; i < myarray.GetLength(0); i++)
 {
     double avg = 0;
     for (int j = 0; j < myarray.GetLength(1); j++)
     {
-        avg = avg + myarray[j,i];
-                       
+        avg = avg + myarray[j, i];
+
     }
-    Console.Write((Math.Round(avg/array[0], 1))+"; ");
+    Console.Write((Math.Round(avg / array[0], 1)) + "; ");
 }
